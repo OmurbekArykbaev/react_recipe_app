324ed8696628fabe676f05e4112f56088e5cbed2
Omurbek Arykbaev <firefoxer00@gmail.com>
2022-03-13 16:00:37 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  // fetch("spoonacular.com/api&key=${process.env.}")
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
